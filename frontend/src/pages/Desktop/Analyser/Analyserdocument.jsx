c9638ab6d2dddf5928625aa5c2a781a260a832ba
root <root@srv1796376.hstgr.cloud>
2026-07-04 11:59:11 +0000

    Server side change in Analyserdocument.jsx

--- frontend/src/pages/Desktop/Analyser/Analyserdocument.jsx
+++ frontend/src/pages/Desktop/Analyser/Analyserdocument.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://acxxelbidding.com/api";
 
 const ANALYSER_DOCS = [
   {
